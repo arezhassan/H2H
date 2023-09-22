a3638559ad51df425f5159a3fea327937d45956e
Arez Hassan <arezimran@gmail.com>
2023-09-22 20:56:34 +0500

    new animation added

--- app/src/main/java/com/example/h2h/SplashScreen.java
+++ app/src/main/java/com/example/h2h/SplashScreen.java
@@ -13,6 +13,7 @@ public class SplashScreen extends AppCompatActivity {
     Animation fadein;
     Animation slide_down;
     Animation slide_up;
+    Animation pulse_anim;
     TextView tvMotto, tvH2H;
     ImageView imgLogo;
 
@@ -24,13 +25,15 @@ public class SplashScreen extends AppCompatActivity {
         fadein = AnimationUtils.loadAnimation(this, R.anim.fade_in);
         slide_down = AnimationUtils.loadAnimation(this, R.anim.slide_down);
         slide_up = AnimationUtils.loadAnimation(this, R.anim.slide_up);
+        pulse_anim = AnimationUtils.loadAnimation(this, R.anim.pulse_anim);
+
        // tvMotto = findViewById(R.id.tvMotto);
      //   tvH2H = findViewById(R.id.tvH2H);
         imgLogo = findViewById(R.id.imgLogo);
 
         //    tvMotto.startAnimation(fadein);
    //     tvH2H.startAnimation(fadein);
-        imgLogo.startAnimation(fadein);
+        imgLogo.startAnimation(pulse_anim);
 
         // Delayed transition to the next activity
         new Handler().postDelayed(new Runnable() {
@@ -41,6 +44,6 @@ public class SplashScreen extends AppCompatActivity {
                 startActivity(intent);
                 finish(); // Finish the splash screen activity to prevent going back to it.
             }
-        }, 1200); // 2000 milliseconds (2 seconds)
+        }, 2500); // 2000 milliseconds (2 seconds)
     }
 }
